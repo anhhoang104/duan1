d481d65188ccadf686a8c3b1becf2d0acf25ee66
ngocvufpt <vunnph45523@fpt.edu.vn>
2024-03-23 12:25:11 +0700

    update page

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,15 +1,13 @@
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top bg-dark shadow">
   <div class="container">
-    <a class="navbar-brand" href="index.php">THỜI TRANG VIỆT</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="index.php">Mỹ phẩm Dior</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <!-- search start -->
       <form action="search.php" method="GET" class="d-flex ms-auto">
-        <input class="form-control mr-sm-2" type="text" name="query" placeholder="Tìm kiếm sản phẩm..."
-          aria-label="Search" style="width: 400px;">
+        <input class="form-control mr-sm-2" type="text" name="query" placeholder="Tìm kiếm sản phẩm..." aria-label="Search" style="width: 400px;">
         <button class="btn btn-outline-secondary" type="submit">Tìm kiếm</button>
       </form>
       <!-- search end -->
@@ -36,11 +34,10 @@
         if (isset($_SESSION['auth'])) {
 
 
-          ?>
+        ?>
 
           <li class="nav-item dropdown ml-5">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-              data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               <?= $_SESSION['auth_user']['name']; ?>
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -50,19 +47,18 @@
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="cart.php"><i class="fas fa-shopping-cart"></i> <span id="cart-item"
-                class="badge badge-danger"></span></a>
+            <a class="nav-link" href="cart.php"><i class="fas fa-shopping-cart"></i> <span id="cart-item" class="badge badge-danger"></span></a>
           </li>
-          <?php
+        <?php
         } else {
-          ?>
+        ?>
           <li class="nav-item">
             <a class="nav-link" href="register.php">Đăng Kí</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="login.php">Đăng Nhập</a>
           </li>
-          <?php
+        <?php
         }
         ?>
 
